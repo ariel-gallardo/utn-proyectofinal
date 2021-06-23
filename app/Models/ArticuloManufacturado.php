b4ea42bbd9289c0f51e9dc240e4b010f04393cfd
Ariel Gallardo <ariel.gallardo96@gmail.com>
2021-06-22 21:58:42 -0300

    development# Ingredientes - Cantidad

--- app/Models/ArticuloManufacturado.php
+++ app/Models/ArticuloManufacturado.php
@@ -21,11 +21,15 @@ class ArticuloManufacturado extends Model
     ];
 
     public function ingredientesTrashed(){
-        return $this->hasManyThrough(ArticuloInsumo::class,ArticuloManufacturadoDetalle::class,'articulo_manufacturado_id','id','id', 'articulo_insumo_id')->withTrashedParents();
+        //return $this->hasManyThrough(ArticuloInsumo::class,ArticuloManufacturadoDetalle::class,'articulo_manufacturado_id','id','id', 'articulo_insumo_id')->withTrashedParents();
+        //return $this->hasManyThrough(ArticuloManufacturadoDetalle::class,ArticuloInsumo::class, 'id', 'articulo_manufacturado_id', 'id', 'id')->withTrashedParents();
+        //return $this->belongsToMany(ArticuloInsumo::class,'articulo_manufacturado_detalles', 'articulo_manufacturado_id', 'articulo_insumo_id', 'id', 'id');
+        //return $this->with()->get();
+        return $this->belongsToMany(ArticuloInsumo::class, ArticuloManufacturadoDetalle::class)->withPivot('cantidad')->withTrashed()->select(array('denominacion','articulo_insumos.unidadMedida'));
     }
 
     public function ingredientes(){
-
+        return $this->belongsToMany(ArticuloInsumo::class, ArticuloManufacturadoDetalle::class)->withPivot('cantidad')->select(array('denominacion', 'articulo_insumos.unidadMedida'));
     }
 }
 
